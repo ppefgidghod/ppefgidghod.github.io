6196ebc7067c76884d07ba8bda272897ba18ade0
Your Name <you@example.com>
2018-09-25 20:25:04 +0800

    修改内容

--- object/hearthstone/hearthstone.js
+++ object/hearthstone/hearthstone.js
@@ -1,9 +1,27 @@
-// var menu = document.querySelector('.menu');
-// var menuY = menu.offsetTop;
-// document.body.onscroll = function() {
-// 	if(window.pageYOffset > menuY) {
-// 		menu.style.position = 'fixed';
-// 	} else {
-// 		menu.style.position = 'relative';
-// 	}
-// }
+$("section.message .wrap .left .top span img:first-of-type").click(function (e) { 
+    $("section.message .wrap .left .top>img:first-of-type").show();
+    $("section.message .wrap .left .top>img:nth-of-type(2),section.message .wrap .left .top>img:nth-of-type(3)").hide();
+});
+$("section.message .wrap .left .top span img:nth-of-type(2)").click(function (e) { 
+    $("section.message .wrap .left .top>img:nth-of-type(2)").show();
+    $("section.message .wrap .left .top>img:nth-of-type(1),section.message .wrap .left .top>img:nth-of-type(3)").hide();
+});
+$("section.message .wrap .left .top span img:nth-of-type(3)").click(function (e) { 
+    $("section.message .wrap .left .top>img:nth-of-type(3)").show();
+    $("section.message .wrap .left .top>img:nth-of-type(1),section.message .wrap .left .top>img:nth-of-type(2)").hide();
+});
+$("section.message .wrap .right .top .dow ul li.ab2").click(function (e) { 
+    $("section.message .wrap .right .top .dow ul li.ab2").css("background-position-y",'-179px');
+    $("section.message .wrap .right .top .dow span").css("background-position-y",'-141px');
+    $("section.message .wrap .right .top .dow ul li.ab1,section.message .wrap .right .top .dow ul li.ab3").css("background-position-y",'2px');
+});
+$("section.message .wrap .right .top .dow ul li.ab1").click(function (e) { 
+    $("section.message .wrap .right .top .dow ul li.ab1").css("background-position-y",'-179px');
+    $("section.message .wrap .right .top .dow span").css("background-position-y",'-367px');
+    $("section.message .wrap .right .top .dow ul li.ab2,section.message .wrap .right .top .dow ul li.ab3").css("background-position-y",'2px');
+});
+$("section.message .wrap .right .top .dow ul li.ab3").click(function (e) { 
+    $("section.message .wrap .right .top .dow ul li.ab3").css("background-position-y",'-179px');
+    $("section.message .wrap .right .top .dow span").css("background-position-y",'-253px'); 
+    $("section.message .wrap .right .top .dow ul li.ab1,section.message .wrap .right .top .dow ul li.ab2").css("background-position-y",'2px'); 
+});
